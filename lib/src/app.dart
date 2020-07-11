786ffa6c2abe57f99c077cf010a5de80f350cd1d
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-12 00:28:34 +0300

    Display network images when clicking on button

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_pictures/src/models/image_model.dart';
+import 'package:flutter_pictures/src/widgets/image_list.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -8,6 +13,19 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
@@ -15,14 +33,10 @@ class AppState extends State<App> {
         appBar: AppBar(
           title: Text('Pictures'),
         ),
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;  
-            });
-          },
+          onPressed: fetchImage,
         ),
       ),
     );
